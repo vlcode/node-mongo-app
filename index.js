281e76df6913293a2dd27dc16baf36bda3151e85
vlcode <lee.seva95@gmail.com>
2021-10-11 23:00:31 +0200

    engine registered, views folder created

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// connect packages to vars 
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const PORT = process.env.PORT || 3000 
 
 const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs' // extension names
+})
+
+// for pages rendering. Engine registered
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views') // where pages are from
 
 // with async it is possible to use await 
 async function start(){
