15f641c5d5edd654408eee6c91de71527fb0ef4e
vlcode <lee.seva95@gmail.com>
2021-10-11 22:49:56 +0200

    database template is ready

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3000 
 
 const app = express()
 
-app.listen(PORT, () => {
-    console.log('Server have been started')
-})
+// with async it is possible to use await 
+async function start(){
+    try {
+        await mongoose.connect('', { //connect database
+            useNewUrlParser: true, 
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => { // database is available at this moment 
+            console.log('Server have been started')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
 
